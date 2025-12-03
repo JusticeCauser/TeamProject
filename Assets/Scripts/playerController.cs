d145939b8e7aaa84acb775aec1ce1c3aa6a8d4a6
Chalmers-13 <chalmerscaleb13@gmail.com>
2025-12-02 17:45:01 -0800

    Lecture 1 Commit:
    1) I added the scripts from the first lecture being:
    - playerController
    - cameraController
    - IDamage
    - enemyAI
    
    2) I created a player capsule object, added the playerController script, a player controller component, assigned initial settings, and moved the main camera to be a child of the player for now.
    
    3) I added the cameraController script to the main camera and assigned initial settings.
    
    4) I created layers for the player, player bullet, enemy, and enemy bullet and changed the Physics Settings Collision Matrix to reflect the new layers.
    
    5) I created a ground Plane and Material to be used as a temporary ground level.
    
    6) I created an enemy sphere prefab including the enemyAI script with inital settings.
    
    7) I created a small area around the player with cubes, walls, and enemy spheres to act as a temporary sandbox for the current assets.
    
    8) I created a Sprint Input in the Input Manager and assigned the button of left shift.
    
    9) I created some folders in the assets menu for organizing current and future assets.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.InputSystem.DualShock;
+
+public class playerController : MonoBehaviour
+{
+    [Header("----- Component -----")]
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [Header("----- Stats -----")]
+    [Range(1, 10)] [SerializeField] int HP;
+    [Range(1, 5)] [SerializeField] int speed;
+    [Range(2, 5)] [SerializeField] int sprintMod;
+    [Range(5, 20)] [SerializeField] int jumpSpeed;
+    [Range(1, 3)] [SerializeField] int jumpMax;
+    [Range(15, 50)] [SerializeField] int gravity;
+
+    [Header("----- Guns -----")]
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
+    int jumpCount;
+    int HPOrig;
+
+    float shootTimer;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        shootTimer += Time.deltaTime;
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.yellow);
+
+        if(Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
+
+        if(controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+
+        playerVel.y -= gravity * Time.deltaTime;
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            playerVel.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if(Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
+        }
+    }
+}
