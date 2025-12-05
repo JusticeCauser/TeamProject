e147dc57b979624c4ac9b6b9d9a962373ee6adab
Ookami101 <lissaannlambert@gmail.com>
2025-12-05 15:44:35 -0800

    EnemyAI Guard and Dog
    Just committing so I can pull. Working on Dog and Guard AI and the alert system.

---
+++ Assets/Scripts/enemyAI_Dog.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class enemyAI_Dog : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int FOV;
+
+    [SerializeField] int biteDmg;
+    [SerializeField] int chaseRange;
+
+    Color colorOrig;
+
+    float biteTimer;
+    float angleToPlayer;
+
+
+    //Range in which dog can see player to shoot
+    bool playerInSightRange;
+    //Range in which dog can smell player
+    bool playerInScentRange;
+
+    Vector3 playerDir;
+    void Start()
+    {
+        colorOrig = model.material.color;
+    }
+
+    void Update()
+    {
+        biteTimer += Time.deltaTime;
+
+        if (playerInSightRange && canSeePlayer())
+        {
+
+        }
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - transform.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, playerDir, out hit))
+        {
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInSightRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInSightRange = false;
+        }
+    }
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    void bite()
+    {
+
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+}
