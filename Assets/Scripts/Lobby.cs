c3063f8cd8dedfdc1a279c6000819cd3bc173156
Kaitlynn Faltermeier <K.Faltermeier99@outlook.com>
2026-01-08 20:51:58 -0600

    created mansion loading scene, added to build profile, along with some more fonts

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -5,10 +5,16 @@ public class Lobby : MonoBehaviour
 {
 
     [SerializeField] string asylumScene = "LoadingSceneAsylum";
+    [SerializeField] string mansionScene = "LoadingSceneManison";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void LoadSceneAsylum()
+    public void loadSceneAsylum()
     {
         SceneManager.LoadScene(asylumScene);
+    }
+
+    public void loadSceneMansion()
+    {
+        SceneManager.LoadScene(mansionScene);
     }
 }
