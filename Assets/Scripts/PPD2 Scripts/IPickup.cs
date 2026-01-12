ef3b5f76962e33c63e0e5d2a6c5247f706e249b4
Kaitlynn Faltermeier <K.Faltermeier99@outlook.com>
2026-01-11 19:08:18 -0600

    in playerController commented out all poison, keylist functions, select gun, IEnumators pertaining to guns, traps, wall running

--- Assets/Scripts/PPD2 Scripts/IPickup.cs
+++ Assets/Scripts/PPD2 Scripts/IPickup.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 
 public interface IPickup
 {
-    public void getGunStats(gunStats gun);
-    public void addKey(keyFunction key);
+   // public void getGunStats(gunStats gun);
+    //public void addKey(keyFunction key);
 }
