908dc9a4af322e25201af492b7940dff346fa522
Kaitlynn Faltermeier <K.Faltermeier99@outlook.com>
2026-01-12 19:50:09 -0600

    attempting to get audio sliders to control the audio.. not successful yet

---
+++ Assets/Scripts/HeistSim Scripts/audioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public static audioManager instance;
+
+    [Header("-----Audio Sources-----")]
+    [SerializeField] AudioSource ambientAudio;
+    [SerializeField] AudioSource sfxAudio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Start()
+    {
+        setVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setVolume();
+    }
+
+    public void setVolume()
+    {
+        if(SettingsManager.instance == null) return;
+
+        if (ambientAudio != null)
+        {
+            ambientAudio.volume = SettingsManager.instance.masterVolume * SettingsManager.instance.ambientVolume;
+        }
+
+    }
+   
+
+}
