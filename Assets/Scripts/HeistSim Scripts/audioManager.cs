b86138330c82e1e1ad0f253d11f1df7d059878a6
Kaitlynn Faltermeier <K.Faltermeier99@outlook.com>
2026-01-14 10:54:57 -0600

    added audio assets, added to switch case for every scene

--- Assets/Scripts/HeistSim Scripts/audioManager.cs
+++ Assets/Scripts/HeistSim Scripts/audioManager.cs
@@ -7,22 +7,27 @@ public class audioManager : MonoBehaviour
 {
     public static audioManager instance;
 
-    public enum ambientType { none, asylum, mansion}
+    public enum ambientType { none, intro, asylum, mansion}
     ambientType curr = ambientType.none;
 
-    [Header("----Audio Settings-----")]
+    [Header("---Audio Settings---")]
     public float masterVolume = 1f;
     public float ambientVolume = 1f;
     public float sfxVolume = 1f;
 
-    [Header("-----Audio Sources-----")]
+    [Header("---Audio Sources---")]
     [SerializeField] AudioSource ambientAudio;
     [SerializeField] AudioSource sfxAudio;
 
-    [Header("------Audio Clips-----")]
+    [Header("----Audio Clips---")]
+    [SerializeField] AudioClip introClip;
     [SerializeField] AudioClip asylumClip;
     [SerializeField] AudioClip mansionClip;
 
+
+    [Header("---SFX Clips---")]
+    [SerializeField] AudioClip bark;
+    [SerializeField] AudioClip footsteps;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
@@ -51,18 +56,7 @@ public class audioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //setVolume();
-    }
-    public void setAmbientVolume(float volume)
-    {
-        ambientVolume = volume;
-
-        PlayerPrefs.SetFloat("Ambient Volume", volume); //set to what player chose
-        PlayerPrefs.Save();
-
-
-        setVolume();
-
+        
     }
     public void setVolume()
     {
@@ -87,8 +81,13 @@ public class audioManager : MonoBehaviour
         
         switch (type)
         {
+
             case ambientType.none:
                 return;
+
+            case ambientType.intro:
+                ambientAudio.clip = introClip;
+                break;
 
             case ambientType.asylum:
                 ambientAudio.clip = asylumClip;
@@ -110,6 +109,10 @@ public class audioManager : MonoBehaviour
     {
         switch (scene.name)
         {
+            case "IntroScene":
+                ambientAudioType(ambientType.intro);
+                break;
+
             case "LoadingIntroAsylum":
             case "Asylum":
                 ambientAudioType(ambientType.asylum);
