216a3968f729c2ba2e52c3481007031724f36c49
Kaitlynn Faltermeier <K.Faltermeier99@outlook.com>
2026-01-14 14:08:59 -0600

    settingsmanager change, it is no longer a child of introui and will work throughout scenes. cannot access currently have not set it up with esc. commented out debug liens will remove on next push

--- Assets/Scripts/HeistSim Scripts/introButtons.cs
+++ Assets/Scripts/HeistSim Scripts/introButtons.cs
@@ -6,27 +6,16 @@ public class introButtons : MonoBehaviour
 
     [SerializeField] string lobby = "Lobby";
     
-    [SerializeField] GameObject introSettings;
+    
 
     private void Start()
     {
-        if(introSettings != null)
-            introSettings.SetActive(false);
+        
     }
     public void Play()
     {
         SceneManager.LoadScene(lobby);
 
-    }
-    public void openSettings()
-    {
-        if(introSettings != null) 
-            introSettings.SetActive(true);
-    }
-    public void closeSettings()
-    {
-        if(introSettings != null) 
-            introSettings.SetActive(false);
     }
     public void Quit()
     {
