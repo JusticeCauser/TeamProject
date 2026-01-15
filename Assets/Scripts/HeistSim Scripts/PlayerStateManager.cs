8749fd374cf33c0ad1eb8c71ea0292ce3e7e0716
Ookami101 <lissaannlambert@gmail.com>
2026-01-14 23:33:33 -0800

    Committing to Pull and Pushing PlayerStateController
    Pushing the current state controller. isHiding will be expanded upon once the hiding system is in place, but now the EnemyAI can hopefully begin properly interacting with the playerState.

--- Assets/Scripts/HeistSim Scripts/PlayerStateManager.cs
+++ Assets/Scripts/HeistSim Scripts/PlayerStateManager.cs
@@ -14,24 +14,134 @@ public enum playerState
 }
 public class PlayerStateManager : MonoBehaviour
 {
-    public playerState CurrentState { get; private set; } = playerState.Idle;
-    
-    public bool isIdle;
-    public bool isSneaking;
-    public bool isSprinting;
-    public bool isCrouching;
-    public bool isHiding;
-    public bool isMoving;
+    public playerState currentState = playerState.Idle;
+
+    bool isCrouching;
+
+    bool isHiding;
+
+    bool isMoving;
+    public float speedMult { get; private set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        crouching();
+        moving();
+
+        switch (currentState)
+        {
+            case playerState.Idle:
+                if (isCrouching == true)
+                {
+                    currentState = playerState.CrouchedIdle;
+                    if (isMoving == true)
+                    {
+                        currentState = playerState.Sneaking;
+                    }
+                }
+                else if (isMoving == true)
+                {
+                    currentState = playerState.Moving;
+                    if (Input.GetButton("Sprint") && isCrouching != true)
+                    {
+                        currentState = playerState.Sprinting;
+                    }
+                }
+                break;
+
+            case playerState.CrouchedIdle:
+                if (isCrouching != true && isMoving != true)
+                {
+                    currentState = playerState.Idle;
+                }
+                else if (isCrouching != true && isMoving == true)
+                {
+                    currentState = playerState.Moving;
+                }
+                else if (isCrouching == true && isMoving == true)
+                {
+                    currentState = playerState.Sneaking;
+                }
+                break;
+
+            case playerState.Sneaking:
+
+                if (isCrouching != true && isMoving != true)
+                {
+                    currentState = playerState.Idle;
+                }
+                else if (isCrouching == true && isMoving != true)
+                {
+                    currentState = playerState.CrouchedIdle;
+                }
+                else if (isCrouching != true && isMoving == true)
+                {
+                    currentState = playerState.Moving;
+                }
+                break;
+
+            case playerState.Sprinting:
+                if (isMoving != true)
+                {
+                    currentState = playerState.Idle;
+                }
+                else if (isCrouching == true && isMoving == true)
+                {
+                    currentState = playerState.Sneaking;
+                }
+                else if (isCrouching != true && isMoving == true)
+                {
+                    currentState = playerState.Moving;
+                }
+                break;
+
+            case playerState.Moving:
+                if (isMoving == false)
+                {
+                    currentState = playerState.Idle;
+                }
+                else if (isCrouching == true && isMoving == true)
+                {
+                    currentState = playerState.Sneaking;
+                }
+                else if (Input.GetButton("Sprint"))
+                {
+                    currentState = playerState.Sprinting;
+                }
+                break;
+
+            case playerState.Hiding:
+
+                break;
+        }
+
+    }
+
+    void crouching()
+    {
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = !isCrouching;
+        }
+    }
+
+    void moving()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
     }
 }
+
