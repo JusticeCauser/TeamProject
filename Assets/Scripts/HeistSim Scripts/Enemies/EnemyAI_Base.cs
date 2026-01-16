e5f097faab0d22bea731e12e78322ae8b8674dd0
Ookami101 <lissaannlambert@gmail.com>
2026-01-16 08:42:51 -0800

    Committing to Pull
    Added in hearing sphere to guards.

--- Assets/Scripts/HeistSim Scripts/Enemies/EnemyAI_Base.cs
+++ Assets/Scripts/HeistSim Scripts/Enemies/EnemyAI_Base.cs
@@ -1,26 +1,30 @@
 using System.Drawing;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 using static enemyAI_Guard;
 
 public class EnemyAI_Base : MonoBehaviour
 {
+    //This allows the Enemies to interact with the playerStateManager
+    protected PlayerStateManager playerStateManager;
+    //Here we have our basics: Model, ability to move, and the ability to dance and preform other movements
     [SerializeField] Renderer model;
     [SerializeField] protected NavMeshAgent agent;
     [SerializeField] Animator anim;
-
+    //These fields allow the enemy to have speed, sight, and other important things - like time spent alert or searching
     [SerializeField] int animTranSpeed;
-    [SerializeField] int HP;
-    [SerializeField] int maxHP;
     [SerializeField] int faceTargetSpeed;
     [SerializeField] int FOV;
     [SerializeField] int roamDist;
     [SerializeField] int roamPauseTime;
     [SerializeField] float alertDur;
-
+    [SerializeField] float searchDur;
     [SerializeField] float roamSpeed;
     [SerializeField] float chaseSpeed;
+    [SerializeField] float baseHearing;
 
+    float searchTimer;
     float roamTimer;
     float angleToPlayer;
     float stoppingDistOrig;
@@ -30,6 +34,7 @@ public class EnemyAI_Base : MonoBehaviour
     {
         Idle,
         Patrol,
+        Search,
         Alerted,
         Chase
     }
@@ -37,29 +42,26 @@ public class EnemyAI_Base : MonoBehaviour
     public guardState state = guardState.Idle;
 
     bool playerInSightRange;
+    [HideInInspector] public bool playerInHearingRange;
 
     Vector3 playerDir;
     Vector3 alertTargetPos;
     Vector3 alertLookDir;
     Vector3 lastAlertPosition;
+    Vector3 lastHeardPosition;
     Vector3 startingPos;
 
     protected Transform playerTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        maxHP = HP;
-
-        if (difficultyManager.instance != null)
-        {
-            HP = Mathf.RoundToInt(HP * difficultyManager.instance.GetHealthMultiplier());
-        }
-
         startingPos = transform.position;
         stoppingDistOrig = agent.stoppingDistance;
 
         if (gameManager.instance.player != null)
             playerTransform = gameManager.instance.player.transform;
+
+        playerStateManager = playerTransform.GetComponent<PlayerStateManager>();
     }
 
     // Update is called once per frame
@@ -179,6 +181,16 @@ public class EnemyAI_Base : MonoBehaviour
         }
 
         return false;
+    }
+    public bool canHearPlayer()
+    {
+        if(playerTransform == null) return false;
+
+        Vector3 playerPos = playerTransform.position;
+        playerDir = playerPos - transform.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        return true;
     }
     private void OnTriggerEnter(Collider other)
     {
