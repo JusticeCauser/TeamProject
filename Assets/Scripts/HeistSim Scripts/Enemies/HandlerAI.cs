a89097df7f65448b2e3cce4ec902193451adaf9e
Ookami101 <lissaannlambert@gmail.com>
2026-01-17 12:12:09 -0800

    Committing to Pull
    Creation of Alert System, Dog, and Handler scripts

--- Assets/Scripts/HeistSim Scripts/Enemies/HandlerAI.cs
+++ Assets/Scripts/HeistSim Scripts/Enemies/HandlerAI.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using static enemyAI_Guard_Handler;
 
 public class HandlerAI : EnemyAI_Base
 {
@@ -7,11 +9,31 @@ public class HandlerAI : EnemyAI_Base
     [SerializeField] enemyAI_Dog dog;
     [SerializeField] enemyAI_Dog dog2;
 
+    Vector3 alertTargetPos;
+    Vector3 alertLookDir;
     public enum guardType
     {
         Handler,
         Elite_Handler
     }
 
+    void onBarkAlert(Vector3 position, Vector3 anchor)
+    {
+        alertTargetPos = position;
 
+        Vector3 playerDir = anchor;
+        playerDir.y = 0;
+
+        if (playerDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion rot = Quaternion.LookRotation(playerDir);
+            transform.rotation = rot;
+        }
+        //moves guard toward anchor
+        agent.stoppingDistance = 0;
+        agent.SetDestination(alertTargetPos);
+        //sets state to alerted
+        state = guardState.Alerted;
+        alertedTimer = 0;
+    }
 }
