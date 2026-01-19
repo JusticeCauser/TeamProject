4078963e62c6d1b21ce5655043e58b04c895ec17
Ookami101 <lissaannlambert@gmail.com>
2026-01-19 12:17:16 -0800

    Dogs Capture
    Edited the dogs so their trigger was actually checked and made sure the capture script properly called capture for the dogs. Dogs can now capture

--- Assets/Scripts/HeistSim Scripts/Enemies/CaptureTrigger.cs
+++ Assets/Scripts/HeistSim Scripts/Enemies/CaptureTrigger.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class CaptureTrigger : MonoBehaviour
 {
     EnemyAI_Base enemyParent;
+    DogAI enemyDog;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         enemyParent = GetComponentInParent<EnemyAI_Base>();
+        enemyDog = GetComponentInParent<DogAI>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("I bit you");
         if (!other.CompareTag("Player")) return;
 
         if (enemyParent != null)
             enemyParent.Capture(other.gameObject);
+        if(enemyDog != null)
+            enemyDog.bite(other.gameObject);
     }
 
     private void OnTriggerStay(Collider other)
@@ -24,5 +27,7 @@ public class CaptureTrigger : MonoBehaviour
 
         if (enemyParent != null)
             enemyParent.Capture(other.gameObject);
+        if(enemyDog != null)
+            enemyDog.bite(other.gameObject);
     }
 }
