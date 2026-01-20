4e2cf034ef887666b7c88f2e33b12a9fac91ec1f
levim <levi.miller1497@gmail.com>
2026-01-19 20:53:29 -0500

    //

--- Assets/Scripts/HeistSim Scripts/pickupItems.cs
+++ Assets/Scripts/HeistSim Scripts/pickupItems.cs
@@ -1,15 +1,61 @@
 using UnityEngine;
+using TMPro;
 public class pickupItems : MonoBehaviour
 {
     [SerializeField] itemStats item;
-   
+    [SerializeField] TMP_Text promptText;
+
+    private bool playerInRange = false;
+
+    private void Start()
+    {
+        if (promptText != null)
+            promptText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!playerInRange || item == null)
+            return;
+
+        if(promptText != null)
+        {
+            promptText.text = "Press E to take " + item.itemName + " ($" + item.itemValue + ")";
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerController player = PlayerController.instance;
+
+            if (player != null)
+            {
+                player.grabItem(item);
+
+                if (promptText != null)
+                    promptText.gameObject.SetActive(false);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        PlayerController player = other.GetComponent<PlayerController>();
-
-        if (player != null)
+        if(other.CompareTag("Player"))
         {
-           player.grabItem(item);
+            playerInRange = true;
+            if (promptText != null)
+                promptText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            if (promptText != null)
+                promptText.gameObject.SetActive(false);
         }
     }
 }
